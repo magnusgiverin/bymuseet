cd2114107304510abbe6ca71db37feec6452abea
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-12 15:32:15 +0200

    Fix: Fix navbar bottom

--- src/bymuseet/src/app/Components/Navbar.tsx
+++ src/bymuseet/src/app/Components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className='p-2 fixed sm:sticky sm:top-0 bottom-0 sm:bottom-auto z-50 font-semibold w-full bg-white md:bg-opacity-75 backdrop-blur-xl'>
       <div className="flex justify-center">
-        <div className="xs:hidden flex items-center">
+      <div className="xs:hidden flex flex-row w-full sm:w-auto justify-between sm:justify-between sm:items-center">
           {shortcuts.map((shortcut, index) => {
             const isActive = pathname === shortcut.url;
             return (
