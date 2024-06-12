36a03a99ad6458e8aa3826a98ec71a9563235a38
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-12 13:51:56 +0200

    Fix: Fix margins on title + subtitle

--- src/bymuseet/src/app/Components/Title.tsx
+++ src/bymuseet/src/app/Components/Title.tsx
@@ -8,9 +8,9 @@ interface TitleProps {
 export const Title: React.FC<TitleProps> = ({ text, type }) => {
     return (
         <h1 className="flex items-center justify-center text-2xl font-extrabold mb-8">
-            <span className={`sm:flex-grow sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
-            <span className="mx-4 whitespace-pre">{text}</span>
-            <span className={`sm:flex-grow sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className={`sm:flex-grow sm:mr-4 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className="whitespace-pre">{text}</span>
+            <span className={`sm:flex-grow sm:ml-4 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
         </h1>
     );
 };
@@ -18,8 +18,8 @@ export const Title: React.FC<TitleProps> = ({ text, type }) => {
 export const SubTitle: React.FC<TitleProps> = ({ text, type = 'green' }) => {
     return (
         <h2 className="flex items-center justify-center text-xl font-bold mb-8">
-            <span className="mr-4 whitespace-pre">{text}</span>
-            <span className={`sm:flex-grow sm:h-0.5 ${type === 'white' ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className="whitespace-pre">{text}</span>
+            <span className={`sm:flex-grow sm:h-0.5 sm:ml-4 ${type === 'white' ? 'bg-white' : 'bg-green-800'}`}></span>
         </h2>
     );
 };
