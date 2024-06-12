2f5189ec7fbe155ee05ce03ad64bd643c9eea370
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-12 21:11:24 +0200

    Add: Add shortname to title

--- src/bymuseet/src/app/Components/Title.tsx
+++ src/bymuseet/src/app/Components/Title.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 interface TitleProps {
     text: string;
+    shortText?: string;
     type?: string;
 }
 
@@ -10,20 +11,23 @@ export const Title: React.FC<TitleProps> = ({ text, type }) => {
 
     return (
         <h1 className="flex flex-wrap items-center justify-center text-2xl font-extrabold mb-8">
-            <span className={`sm:flex-grow sm:mr-10 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className={`sm:flex-grow sm:mr-4 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
             <span className="whitespace-pre-wrap text-center">{spacedText}</span>
-            <span className={`sm:flex-grow sm:ml-10 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className={`sm:flex-grow sm:ml-4 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
         </h1>
     );
 };
 
-export const SubTitle: React.FC<TitleProps> = ({ text, type = 'green' }) => {
-    const spacedText = text.toUpperCase().split('').join('  ');
+export const SubTitle: React.FC<TitleProps> = ({ text, shortText, type = 'green' }) => {
+    const spacedText = (text: string) => text.toUpperCase().split('').join('  ');
 
     return (
         <h2 className="flex flex-wrap items-center justify-center text-xl font-bold mb-8">
-            <span className="whitespace-pre-wrap text-center">{spacedText}</span>
-            <span className={`sm:flex-grow sm:h-0.5 sm:ml-10 ${type === 'white' ? 'bg-white' : 'bg-green-800'}`}></span>
+            <span className="whitespace-pre-wrap text-center">
+                <span className="block sm:hidden">{spacedText(shortText || text)}</span>
+                <span className="hidden sm:block">{spacedText(text)}</span>
+            </span>
+            <span className={`sm:flex-grow sm:h-0.5 sm:ml-4 ${type === 'white' ? 'bg-white' : 'bg-green-800'}`}></span>
         </h2>
     );
 };
--- src/bymuseet/src/app/lokaliteter/[location-id]/page.tsx
+++ src/bymuseet/src/app/lokaliteter/[location-id]/page.tsx
@@ -54,12 +54,12 @@ export default function Lokalitet() {
             {location ? (
                 <>
                     <div className="flex flex-col md:flex-row">
-                        <div className="bg-white p-2 md:p-8 shadow-md md:w-2/3 w-full md:rounded-s-md">
+                        <div className="bg-white py-8 px-2 md:p-8 shadow-md lg:w-2/3 w-full md:rounded-s-md">
                             <SubTitle text={location.name} />
                             <p className="whitespace-pre-wrap">{location.description}</p>
                             <Button icon={'ArrowLeft_sm'} text={'tilbake'} iconPosition='left' />
                         </div>
-                        <div className="md:h-auto h-60 md:w-1/3 w-full relative shadow-md">
+                        <div className="md:h-auto h-60 lg:w-1/3 w-full relative shadow-md">
                             <Image
                                 src={location.image}
                                 alt="Fasade Levanger"
@@ -70,7 +70,7 @@ export default function Lokalitet() {
                         </div>
                     </div>
                     {images.length !== 0 && (
-                        <div className="my-10 bg-white rounded-0 md:rounded-md md:p-8 p-2 ">
+                        <div className="my-10 bg-white rounded-0 md:rounded-md md:p-8 py-8 px-2 ">
                             <SubTitle text='album' />
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
                                 {images.map((image, index) => (
@@ -92,7 +92,7 @@ export default function Lokalitet() {
                 <p>Location not found</p>
             )}
             <div className="mt-10 bg-white rounded-0 md:rounded-md p-8 ">
-                <SubTitle text='utforsk videre' />
+                <SubTitle text='utforsk videre' shortText='utforsk' />
                 <div className="flex flex-row flex-wrap justify-center gap-8">
                     {locations.filter(loc => loc.id !== location?.id).map((loc) => (
                         <div key={loc.id} className="flex flex-col items-center transition-transform duration-300 ease-in-out transform hover:scale-105">
