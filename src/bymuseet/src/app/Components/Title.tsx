007ef7158a95f3ce55fc629b3a014f7b0b05c773
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-14 23:53:06 +0200

    Fix: Fix some styling

--- src/bymuseet/src/app/Components/Title.tsx
+++ src/bymuseet/src/app/Components/Title.tsx
@@ -12,7 +12,7 @@ export const Title: React.FC<TitleProps> = ({ text, shortText, type }) => {
     return (
         <h1 className="flex items-center justify-center text-base text-lg lg:text-xl font-bold mb-8">
             <span className={`sm:flex-grow sm:mr-4 sm:h-0.5 ${type === "white" ? 'bg-white' : 'bg-green-800'}`}></span>
-            <span className="whitespace-pre-wrap text-center text-green-800 md:text-black">
+            <span className="whitespace-pre-wrap text-center">
                 <span className="block sm:hidden">{spacedText(shortText || text)}</span>
                 <span className="hidden sm:block">{spacedText(text)}</span>
             </span>
@@ -24,7 +24,7 @@ export const Title: React.FC<TitleProps> = ({ text, shortText, type }) => {
 export const SubTitle: React.FC<TitleProps> = ({ text, shortText, type = 'green' }) => {
     return (
         <h2 className="flex items-center justify-center text-base text-lg lg:text-xl font-semibold mb-8">
-            <span className="whitespace-pre-wrap text-center text-green-800 md:text-black">
+            <span className="whitespace-pre-wrap text-center">
                 <span className="block sm:hidden">{spacedText(shortText || text)}</span>
                 <span className="hidden sm:block">{spacedText(text)}</span>
             </span>
