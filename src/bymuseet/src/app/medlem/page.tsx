bc6a4f42be6771ca1e4875de38d8688d7c6fbe6d
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-17 20:34:35 +0200

    Update: Update member page

--- src/bymuseet/src/app/medlem/page.tsx
+++ src/bymuseet/src/app/medlem/page.tsx
@@ -4,16 +4,31 @@ import { Title } from "../Components/Title";
 
 export default function Medlem() {
     return (
-        <main className="mt-28 py-8 px-4 md:p-8 flex flex-col items-center md:rounded-md justify-center bg-white gap-2 w-full">
-            <span className="w-full">
-            <Title text="Takk for din interesse" shortText="takk"/>
-            </span>
-            <p className="text-center">Vi setter stor pris på at du ønsker å bli medlem hos oss.</p>
-            <p className="text-center">For å få mer informasjon, kontakt oss på:</p>
-            <p className="mt-4 text-blue-500">
-                <a href="mailto:bymuseetilevanger@gmail.com">bymuseetilevanger@gmail.com</a>
-            </p>
-            <Button icon={"ArrowLeft_sm"} text={"tilbake"} iconPosition="left"/>
-        </main>
+        <main className="flex flex-col lg:flex-row gap-5 md:gap-10 w-full my-5 md:my-10">
+            <div className="w-full lg:w-2/3 h-auto bg-white p-8 md:rounded-md flex flex-col py-8 px-4">
+                <span className="w-full">
+                    <Title text="Takk for din interesse" shortText="takk" />
+                </span>
+                <p className="flex-grow">Bymuseet drives på frivillig basis og uten faste tilskudd til drift og vedlikehold. Det kreves årlig betydelige beløp til vedlikehold av de verneverdige bygningene. Dessuten er det nødvendig med midler for å kunne drive museumsarbeid i form av utstillinger og ulike arrangementer gjennom hele året. Gjennom din støtte til Bymuseet er du/dere med å bevare hus og sikre opplevelser som del av kulturarvmiljøet i Levanger.</p>
+                <div className="mt-auto">
+                    <Button icon={"ArrowLeft_sm"} text={"tilbake"} iconPosition="left" />
+                </div>
+            </div>
+            <div className="w-full lg:w-1/3 bg-white py-8 px-4 md:rounded-md flex flex-col items-center">
+                <div className="w-full flex flex-col items-center justify-center text-center">
+                    <p className="mt-4 font-bold">Medlem: 300 kr pr år</p>
+                    <p className="font-bold">Bedrift: 700 kr pr år</p>
+                    <p className="mt-4 font-bold">VIPPS til 506171 eller betal til konto 4420.06.97283</p>
+                    <p>HUSK skriv navn og epostadresse</p>
+                    <p className="mt-4 font-bold">TAKK FOR DIN STØTTE</p>
+                </div>
+                <div className="w-full flex flex-col items-center p-8 text-center">
+                    <p>For å få mer informasjon, kontakt oss på:</p>
+                    <p className="mt-4 text-blue-500">
+                        <a href="mailto:bymuseetilevanger@gmail.com">bymuseetilevanger@gmail.com</a>
+                    </p>
+                </div>
+            </div>
+        </main >
     );
 }
