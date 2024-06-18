06989d073cae1c7dca69ccbc98b7c884fa534b31
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-18 19:26:30 +0200

    Fix: Fix info box on medlem

--- src/bymuseet/src/app/medlem/page.tsx
+++ src/bymuseet/src/app/medlem/page.tsx
@@ -17,15 +17,13 @@ export default function Medlem() {
                     <Button icon={"ArrowLeft_sm"} text={medlem.buttonText} iconPosition="left" />
                 </div>
             </div>
-            <div className="w-full lg:w-1/3 bg-white py-8 px-4 md:p-8 md:rounded-md">
+            <div className="w-full lg:w-1/3 h-auto bg-white p-8 md:rounded-md flex flex-col py-8 px-4 md:p-8">
                 <div className="w-full flex flex-col items-center justify-center text-center">
                     <p className="font-bold">{medlem.memberPrice}</p>
                     <p className="font-bold">{medlem.businessPrice}</p>
                     <p className="mt-4 font-bold">{medlem.paymentText}</p>
                     <p>{medlem.reminder}</p>
                     <p className="mt-4 font-bold">{medlem.thankYou}</p>
-                </div>
-                <div className="w-full flex flex-col items-center p-8 text-center">
                     <p>{medlem.information}</p>
                     <p className="mt-4 text-blue-500">
                         <a href={"mailto:" + medlem.emailAddress}>{medlem.emailAddress}</a>
