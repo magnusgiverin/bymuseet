ead5768a70150e8b15b795e0792eabad5a5bea6f
Magnus Giverin <magnusgiverin@icloud.com>
2024-06-24 06:48:57 +0200

    Fix: Fix images and link

--- src/bymuseet/src/app/Utils/data/text.tsx
+++ src/bymuseet/src/app/Utils/data/text.tsx
@@ -64,7 +64,7 @@ export const textContent = {
             links: [
                 { buttonText: "Digital Levangertur", link: "https://www.opplevinnherred.no/levanger/digital-tur-levanger/" },
                 { buttonText: "Levanger som fredet by", link: "https://riksantikvaren.no/kulturhistorie/levanger-ein-historisk-og-levande-by/" },
-                { buttonText: "Den kulturelle skolesekken", link: "https://www.denkulturelleskolesekken.no/trondelag/" }
+                { buttonText: "Den kulturelle skolesekken", link: "https://www.denkulturelleskolesekken.no/utforsk-produksjoner-i-dks/?produksjon=15321" }
             ]
         }
     },
