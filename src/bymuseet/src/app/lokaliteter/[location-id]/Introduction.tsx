9bd71aefeb6d6700c07fd909102b87cc70577fde
Magnus Giverin <magnusgiverin@icloud.com>
2024-07-27 22:18:54 +0200

    Fix: Fix width of the thingy picture on the introduction for the locations page

--- src/bymuseet/src/app/lokaliteter/[location-id]/Introduction.tsx
+++ src/bymuseet/src/app/lokaliteter/[location-id]/Introduction.tsx
@@ -10,12 +10,12 @@ interface IntroductionProps {
 const Introduction: React.FC<IntroductionProps> = ({ location }) => {
     return (
         <div className="w-full flex flex-col lg:flex-row my-5 md:my-10">
-            <div className="bg-white py-8 px-4 md:p-8 shadow-md lg:w-2/3 xl:w-3/5 w-full md:rounded-s-md">
+            <div className="bg-white py-8 px-4 md:p-8 shadow-md lg:w-1/2 xl:w-3/5 w-full md:rounded-s-md">
                 <Title text={location.name} />
                 <p className="whitespace-pre-wrap">{location.description}</p>
                 <Button icon={'ArrowLeft_sm'} text={'tilbake'} iconPosition='left' />
             </div>
-            <div className="lg:h-auto h-60 lg:w-1/3 xl:w-2/5 w-full relative shadow-md">
+            <div className="lg:h-auto h-60 lg:w-1/2 xl:w-2/5 w-full relative shadow-md">
                 <Image
                     src={location.image}
                     alt="Fasade Levanger"
